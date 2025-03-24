ffef8764d404002c97b5d22ad9cfba04d92fd61a
Chinmay-Prashanth <cp3873@nyu.edu>
2025-03-24 16:39:21 +0530

    Remove build, install, and log directories from git tracking

--- build/tt_robot_new/CMakeFiles/tt_robot_new_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tt_robot_new_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tt_robot_new_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/tt_robot_new/CMakeFiles/tt_robot_new_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for tt_robot_new_uninstall.
-# This may be replaced when dependencies are built.
